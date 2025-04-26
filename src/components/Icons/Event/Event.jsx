a8621a57d6d5c8a1d01506bcfbca7ba6c358baf0
Evgeniy <evgnec@gmail.com>
2025-04-26 18:35:14 +0300

    upd

--- src/components/Icons/Event/Event.jsx
+++ src/components/Icons/Event/Event.jsx
@@ -1,29 +1,46 @@
 import css from './Event.module.css';
 import PropTypes from 'prop-types';
+import {formatEventStart} from '../../../Utils/FormatEventStart.js';
+import {formatEventDuration} from '../../../Utils/FormatEventDuration.js';
+import {
+  FaMapMarkerAlt,
+  FaUserAlt,
+  FaCalendarAlt,
+  FaClock,
+} from 'react-icons/fa';
 
-export const Event = ({name, location, speaker, type, start, end}) => {
-    return <div className={css.event}>
-    <h2 className={css.title}>{name}</h2>
-    <p className={css.info}>
-        <i className={css.icon}>Location</i>
-    </p>
-    <p className={css.info}>
-        <i className={css.icon}>Speaker</i>
-    </p>
-    <p className={css.info}>
-        <i className={css.icon}>Start Date</i>
-    </p>
-    <p className={css.info}>
-        <i className={css.icon}>Duration</i>
-    </p>
-   <span className={css.chip}>Event type</span>
-    </div>;
-}
+export const Event = ({ name, location, speaker, type, start, end }) => {
+    const formattedStart= formatEventStart(start);
+    const Duration = formatEventDuration(start,end);
+  return (
+    <div className={css.event}>
+      <h2 className={css.title}>{name}</h2>
+      <p className={css.info}>
+        <FaMapMarkerAlt className={css.icon} size={16} />
+        {location}
+      </p>
+      <p className={css.info}>
+        <FaUserAlt className={css.icon} size={16} />
+        {speaker}
+      </p>
+      <p className={css.info}>
+        <FaCalendarAlt className={css.icon} size={16} />
+        {formattedStart}
+      </p>
+      <p className={css.info}>
+        <FaClock className={css.icon} size={16} />
+        {Duration}
+      </p>
+      <span className={css.chip}>{type}</span>
+    </div>
+  );
+};
 
 Event.propTypes = {
-    name:PropTypes.string.isRequired, 
-    location:PropTypes.string.isRequired, 
-    speaker:PropTypes.string.isRequired, 
-    type:PropTypes.string.isRequired,
-    start:PropTypes.string.isRequired, 
-    end:PropTypes.string.isRequired}
+  name: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  speaker: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  start: PropTypes.string.isRequired,
+  end: PropTypes.string.isRequired,
+};
