85daa2c60936d8d700cd624525cc407ab615be58
Evgeniy <evgnec@gmail.com>
2025-05-04 00:33:33 +0300

    upd

--- src/components/Cats/Logo/Logo.jsx
+++ src/components/Cats/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import css from './Logo.module.css';
 
 export default function Logo() {
   return (
-    <a href="" className={css.logo}>
+    <a href="#" className={css.logo}>
       <ImAirplane className={css.icon} />
       Cat App Logo
     </a>
